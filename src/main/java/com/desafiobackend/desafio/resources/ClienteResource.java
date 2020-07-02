93be52ca2ba305b5ccf55fa8c732369bfeea1d64
Atila Silva <atila__7@hotmail.com>
2020-07-02 16:19:58 -0300

    Camada de serviço e Registro de Componente

--- src/main/java/com/desafiobackend/desafio/resources/ClienteResource.java
+++ src/main/java/com/desafiobackend/desafio/resources/ClienteResource.java
@@ -1,23 +1,33 @@
 package com.desafiobackend.desafio.resources;
 
-import java.time.Instant;
-import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafiobackend.desafio.entities.Cliente;
-import com.desafiobackend.desafio.entities.enums.Sexo;
+import com.desafiobackend.desafio.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
 	
+	@Autowired
+	private ClienteService service;
+	
 	@GetMapping
-	public ResponseEntity<Cliente> findAll(){
-		Cliente cli = new Cliente(1L, "Atila Izac", Sexo.valueOf("MASCULINO"), Date.from(Instant.parse("1993-12-31T00:00:00Z")));
-		return ResponseEntity.ok().body(cli);
+	public ResponseEntity<List<Cliente>> findAll(){
+		List<Cliente> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Cliente> findById(@PathVariable Long id){
+		Cliente obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
