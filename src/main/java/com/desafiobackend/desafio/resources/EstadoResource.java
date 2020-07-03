5af2f5e2c69f2f0ee536d42f37323889ba4fc8bf
Atila Silva <atila__7@hotmail.com>
2020-07-02 23:08:44 -0300

    EstadoService e EstadoResource

---
+++ src/main/java/com/desafiobackend/desafio/resources/EstadoResource.java
@@ -0,0 +1,33 @@
+package com.desafiobackend.desafio.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafiobackend.desafio.entities.Estado;
+import com.desafiobackend.desafio.services.EstadoService;
+
+@RestController
+@RequestMapping(value = "/estados")
+public class EstadoResource {
+	
+	@Autowired
+	private EstadoService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Estado>> findAll(){
+		List<Estado> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Estado> findById(@PathVariable Long id){
+		Estado obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/desafiobackend/desafio/services/EstadoService.java
@@ -0,0 +1,27 @@
+package com.desafiobackend.desafio.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.desafiobackend.desafio.entities.Estado;
+import com.desafiobackend.desafio.repositories.EstadoRepository;
+import com.desafiobackend.desafio.services.exceptions.ExcecaoDeRecursoNaoEncontrado;
+
+@Service
+public class EstadoService {
+
+	@Autowired
+	private EstadoRepository repository;
+	
+	public List<Estado> findAll(){
+		return repository.findAll();
+	}
+	
+	public Estado findById(Long id) {
+		Optional<Estado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ExcecaoDeRecursoNaoEncontrado(id));
+	}
+}
