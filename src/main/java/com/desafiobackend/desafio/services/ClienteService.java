1ccada4666518db1eacb6f3feafb823c4c798f74
Atila Silva <atila__7@hotmail.com>
2020-07-04 14:50:26 -0300

    Tratamento de exceção para remoção de recurso

--- src/main/java/com/desafiobackend/desafio/services/ClienteService.java
+++ src/main/java/com/desafiobackend/desafio/services/ClienteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,6 +39,10 @@ public class ClienteService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ExcecaoDeRecursoNaoEncontrado(id);
+		}
 	}
 }
